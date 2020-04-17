e2a988cde9f663a9413831117be97347c7a4dec4
waba10 <00189617@uca.edu.sv>
2020-04-16 18:39:29 -0600

    Agregar archivos de proyecto.

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace Gighub.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Gighub.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Gighub.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
