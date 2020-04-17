60ca35b972107b0aec15b4cf3b286cd0fdaf185a
waba10 <00189617@uca.edu.sv>
2020-04-17 00:08:40 -0600

    Show Form

---
+++ Controllers/GigsController.cs
@@ -0,0 +1,26 @@
+﻿using Gighub.Models;
+using Gighub.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Gighub.Controllers
+{
+    public class GigsController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public GigsController(){
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Gigs
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
